a0b245c6344f1175ad8d688ca12b491a2eca8405
Michal Šoltis <msoltis@redhat.com>
2026-01-30 09:48:41 +0100

    prefetch-dependencies: Add a new subcommand
    Add a new cobra.Command with short and long description. Only two steps
    to do:
    
    1. New -> get new prefetch struct with config and Hermeto CLI
    2. Run -> execute "main" function (former Bash in YAML script)
    
    The init() function runs automatically when the package is loaded. The
    same pattern is applied also to other subcommands.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

---
+++ cmd/prefetch_dependencies.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/konflux-ci/konflux-build-cli/pkg/commands/prefetch_dependencies"
+	"github.com/konflux-ci/konflux-build-cli/pkg/common"
+	"github.com/konflux-ci/konflux-build-cli/pkg/logger"
+)
+
+var log = logger.Logger.WithField("logger", "PrefetchDependencies")
+
+var PrefetchDependenciesCmd = &cobra.Command{
+	Use:   "prefetch-dependencies",
+	Short: "Prefetch project dependencies",
+	Long:  "Prefetch project dependencies using Hermeto to enable hermetic container builds",
+	Run: func(cmd *cobra.Command, args []string) {
+		prefetchDependencies, err := prefetch_dependencies.New(cmd)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := prefetchDependencies.Run(); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func init() {
+	common.RegisterParameters(PrefetchDependenciesCmd, prefetch_dependencies.ParamsConfig)
+}
